563be15fe9f1c657a627801df72a0defd23dfac7
Pedro Alcoba <pedralcg@gmail.com>
2026-01-03 16:59:54 +0100

    Part08a - Ejer 8.3 Todos los autores

--- library-backend/index.js
+++ library-backend/index.js
@@ -102,10 +102,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+  name: String!
+  id: ID!
+  born: Int
+  bookCount: Int!
+}
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -114,6 +122,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    // El primer parámetro 'root' contiene los datos del autor que se está procesando
+    bookCount: (root) => {
+      // Filtramos el array de libros por el nombre del autor actual
+      return books.filter((b) => b.author === root.name).length;
+    },
   },
 };
 
