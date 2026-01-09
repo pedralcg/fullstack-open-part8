b8c5e41ae9ccc10e92bb128733636603325959ae
Pedro Alcoba <pedralcg@gmail.com>
2026-01-09 19:27:08 +0100

    Part08d - Ejer 8.19 Libros por género, parte 1

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const [genre, setGenre] = useState("all genres");
   const result = useQuery(ALL_BOOKS);
 
   if (!props.show) {
@@ -14,9 +16,23 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  // Extraer todos los géneros únicos de todos los libros
+  const genres = ["all genres", ...new Set(books.flatMap((b) => b.genres))];
+
+  // Filtrar la lista basada en el estado 'genre'
+  const booksToShow =
+    genre === "all genres"
+      ? books
+      : books.filter((b) => b.genres.includes(genre));
+
   return (
     <div>
       <h2>books</h2>
+      {genre !== "all genres" && (
+        <p>
+          in genre <strong>{genre}</strong>
+        </p>
+      )}
 
       <table>
         <tbody>
@@ -25,7 +41,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {booksToShow.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -34,6 +50,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      {/* Renderizar botones para cada género */}
+      <div>
+        {genres.map((g) => (
+          <button key={g} onClick={() => setGenre(g)}>
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
